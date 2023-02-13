6c7412646cebc227d0db8b677ca810b4572079f8
Dia_Bansal <78194499+Dia-Bansal@users.noreply.github.com>
2023-02-13 23:51:39 +0530

    Add files via upload

---
+++ client/src/data/SliderData.js
@@ -0,0 +1,30 @@
+import ImageOne from "../images/house1.jpg";
+import ImageTwo from "../images/house2.jpg";
+import ImageThree from "../images/house3.jpg";
+
+export const SliderData = [
+  {
+    title: "Luxury Villa in Bali",
+    price: "$520",
+    path: "/homes",
+    label: "View Home",
+    image: ImageOne,
+    alt: "Home",
+  },
+  {
+    title: "Luxury Villa in Vancouver",
+    price: "$620",
+    path: "/homes",
+    label: "View Home",
+    image: ImageTwo,
+    alt: "Home",
+  },
+  {
+    title: "Luxury Villa in India",
+    price: "$720",
+    path: "/homes",
+    label: "View Home",
+    image: ImageThree,
+    alt: "Home",
+  },
+];
